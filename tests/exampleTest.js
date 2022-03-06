3cca47da80e2ecce51a18d05c44c65e776273f8f
lillianaleecode <lilliana.lee@code.berlin>
2022-03-06 19:32:27 +0100

    initial testing with Puppeteer

---
+++ tests/exampleTest.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer')
+describe('My First Puppeteer Test', () => {
+    it('should lauch the browser', async function() {
+        const browser = await puppeteer.launch({headless: false})
+        const page = await browser.newPage() 
+        await page.goto('https://code.berlin/en/')//go to the URL page
+        await page.waitForSelector('p') //find a p tag or throw an error
+        await browser.close() //close browser after finishing the script
+    })
+})
+
+//to run this, we add this in the package.json
+//to run this terminal: npm run test
