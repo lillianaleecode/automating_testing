a68ee3b2264310cde7c8050913748549eefefcf7
lillianaleecode <lilliana.lee@code.berlin>
2022-03-08 15:07:54 +0100

    Accessing JS AdSSetup variable

--- tests/exampleTest.js
+++ tests/exampleTest.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer')
+
 describe('My First Puppeteer Test', () => {
     it('should lauch the browser', async function() {
         const browser = await puppeteer.launch({
@@ -10,12 +11,23 @@ describe('My First Puppeteer Test', () => {
         await page.goto('https://code.berlin/en/')//go to the URL page
         await page.waitForSelector('p') //find a p tag or throw an error
 
-        await page.goto('https://www.businessinsider.de/')
+        await page.goto('https://www.bild.de/')
         const title = await page.title()
         const url = await page.url()
 
         console.log('Title: ' + title)
         console.log('URL: ' + url)
+
+        //const getPTag = await page.$selector('p');
+        //const getInnerHTMLProperty = await getPTag.getProperty('innerHTML');
+        //console.log(getInnerHTMLProperty)
+
+        const ads = await page.evaluate("adSSetup");
+        console.log(ads);
+
+        
+
+    
 
 
         //await page.goBack()
