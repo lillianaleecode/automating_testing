98340c5adfc1682af714b010e4deebcce82639e1
lillianaleecode <lilliana.lee@code.berlin>
2022-03-15 13:18:38 +0100

    retrieving the page in the console

--- tests/exampleTest.js
+++ tests/exampleTest.js
@@ -1,33 +1,62 @@
 const puppeteer = require('puppeteer')
 
-describe('My First Puppeteer Test', () => {
+var url = 'https://www.bild.de';
+
+async function run () {
+
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto(url);
+    
+        const flashVariableName = await page.evaluate( () => {
+            var start = document.documentElement.innerHTML.indexOf('ASCDP') //+ 'ASCDP '.length;
+            var tempDoc = document.documentElement.innerHTML.substring(start);
+            return  tempDoc;
+        });
+    
+            //console.log(await page.evaluate( (flashVariableName) => flashVariableName ));
+            console.log(await page.evaluate( (flashVariableName) => flashVariableName, flashVariableName ));
+    
+            await browser.close();
+            console.log("Fin del testing!!!!!!")
+        } catch(e){
+            console.log("Error Occurred", e)
+        }
+    }
+    
+    run();
+
+describe('Puppeteer for AdTech', () => {
     it('should lauch the browser', async function() {
         const browser = await puppeteer.launch({
             headless: false, 
-            slowMo: 50, 
+            //slowMo: 50, 
             devtools: true
         })
         const page = await browser.newPage() 
-        await page.goto('https://code.berlin/en/')//go to the URL page
-        await page.waitForSelector('p') //find a p tag or throw an error
+       // await page.goto('https://code.berlin/en/')//go to the URL page
+       // await page.waitForSelector('p') //find a p tag or throw an error
 
-        await page.goto('https://www.bild.de/')
+        await page.goto(url)
         const title = await page.title()
-        const url = await page.url()
+        const urlLink = await page.url()
 
         console.log('Title: ' + title)
-        console.log('URL: ' + url)
+        console.log('URL: ' + urlLink)
 
-        //const getPTag = await page.$selector('p');
-        //const getInnerHTMLProperty = await getPTag.getProperty('innerHTML');
-        //console.log(getInnerHTMLProperty)
-
-        const ads = await page.evaluate("adSSetup");
+        const ads = await page.evaluate("adSSetup"); //retrieves the adSetup
         console.log(ads);
 
-        
+       // console.log(await page.evaluate(() => "bild.js"));
 
-    
+        
+        
+        //const adJsFile = await page.content('bild.js'); //retrieves the website but not the bild.js file..
+       // console.log(adJsFile);
 
 
         //await page.goBack()
@@ -39,3 +68,4 @@ describe('My First Puppeteer Test', () => {
 
 //to run this, we add this in the package.json
 //to run this terminal: npm run test
+
