04de10276d1259237ef17afcff653469677cf19c
lillianaleecode <lilliana.lee@code.berlin>
2022-04-10 22:24:00 +0200

    cleaning code

--- takeScreenshots.js
+++ takeScreenshots.js
@@ -94,21 +94,7 @@ describe('Taking Screenshots<3', () => {
         const numberTimes = Math.floor(height/viewportHeight);
         console.log(numberTimes);
 
-        // for (let i = 0; i < numberTimes; i++) {
-
-        //      // force lazy loading
-        //     await page.evaluate(() => window.scrollTo(0,i*200));
-
-        //     await page.screenshot({ 
-        //         //path: `screenshot${Date.now()}.png`,
-        //         path: `${path}/Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //         type: "png",
-        //         timeout: 30000,
-    
-    
-        //      });
-            
-        // }
+       
 
         //this code will scroll until the end of the page (full page)
         async function autoScroll(page){
@@ -129,14 +115,7 @@ describe('Taking Screenshots<3', () => {
                 });
             });
         }
-       // await autoScroll(page);
-//Screenshot by distance (full page)
-        // await page.screenshot({ 
-        //     //path: `screenshot${Date.now()}.png`,
-        //     path: `${path}/Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //     type: "png",
-        //     fullPage: true,
-        //     timeout: 80000,})
+
 
 //scrolling function (for by distance partial)
         async function autoScroll2(page, distance){
@@ -165,102 +144,7 @@ describe('Taking Screenshots<3', () => {
         console.log("hello 3");
         await screenshot2(page);
 
-
-        // await autoScroll(page);
-
-        // await page.screenshot({ 
-        //     //path: `screenshot${Date.now()}.png`,
-        //     path: `${path}/Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //     type: "png",
-        //     timeout: 30000,
-        //     fullPage: true,
-
-        // });
-
-
-        // console.log(numberTimes);
-
-        // //so far this loop takes screenshots from the first 200px high
-        // await (async () => {
-        //     for (let i = 0; i < numberTimes; i++) {
-
-        //         console.log("corrio el loop");
-              
-        //      // force lazy loading
-        //     await page.evaluate(() => window.scrollTo(0,i*200));
-
-        
-
-        //     await page.screenshot({ 
-        //         //path: `screenshot${Date.now()}.png`,
-        //         path: `${path}/Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //         type: "png",
-        //         timeout: 30000,
-
-        //     });
     
-    
-        //     }
-        // })();
-
-
-
-
-        // await page.setViewport({ 
-        //     width: 1600, 
-        //     height: height, 
-        //     //deviceScaleFactor: 1 
-        // });
-
-         //Screenshop Desktop Size
-        
-
-         
-
-
-        // //Screenshop Desktop Size
-        // await page.screenshot({ 
-        //     //path: `screenshot${Date.now()}.png`,
-        //     path: `${path}/Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //     type: "png",
-        //     fullPage: true,
-        //     timeout: 80000,
-
-        //  });
-        //  console.log("se cayo 1")
-
-        //  //partial desktop screenshot with coordinates
-        //  await page.screenshot({
-        //     path: `${path}/Partial Screenshot from desktop ${" " + dateString + " " + Date.now()} .png`,
-        //     'clip': {
-        //         'x': 600, 
-        //         'y': 0, 
-        //         'width': 650, 
-        //         'height': 650}
-        // });   
-        // console.log("se cayo 2")
-
-        //  // Screenshot Emulate of an iPhone X
-        // await page.setUserAgent('Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1');
-        // await page.setViewport({ width: 375, height: 812 });
-        // await page.goto(url);
-
-        // console.log("se cayo 2.5")
-
-        // await page.screenshot({ 
-        //     path: `${path}/Screenshot from mobile ${" " + dateString + " " + Date.now()} .png`,
-        //     type: "png",
-        //     fullPage: true,
-        //     timeout: 80000,
-
-        //  });
-
-        //  console.log("se cayo 3")
-
-         
-         
-  
-        
         const title = await page.title()
         const urlLink = await page.url()
 
@@ -276,6 +160,6 @@ describe('Taking Screenshots<3', () => {
     })
 })
 
-//to run this terminal: npm run test
+//to run this terminal: npm run test takeScreenshots.js
 
 
