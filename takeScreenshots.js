5e578e6daf46f55cd1fd36086c6db75431f5c1c4
lillianaleecode <lilliana.lee@code.berlin>
2022-04-11 12:58:30 +0200

    partial screenshots of Adslots by elementID

--- takeScreenshots.js
+++ takeScreenshots.js
@@ -2,7 +2,8 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer')
 
-var url = 'https://www.bild.de';
+//var url = 'https://www.bild.de';
+var url = 'https://www.bild.de/news/ausland/news-ausland/schweiz-iphone-rettet-abgestuerztem-snowboarder-das-leben-79683022.bild.html'
 
 
 
@@ -16,7 +17,8 @@ describe('Taking Screenshots<3', () => {
         const page = await browser.newPage() 
 
         // Configure the navigation timeout
-        await page.setDefaultNavigationTimeout(30000);
+        //await page.setDefaultNavigationTimeout(0);
+        this.timeout(0);
 
         await page.goto(url, {
             //waitUntil: 'networkidle2', //Wait for all non-lazy loaded images to load. networkidle2 works better than load, domcontentloaded or networkidle0
@@ -27,14 +29,14 @@ describe('Taking Screenshots<3', () => {
         // force lazy loading
         // await page.evaluate(() => window.scrollTo(0, Number.MAX_SAFE_INTEGER));
 
-        await page.waitFor(3000);
+        // await page.waitFor(3000);
         
         //
         // await page
         // .waitForSelector('#red-teaser-image')
         // .then(() => console.log('got it'));
 
-        await page.setDefaultTimeout(30000);
+        await page.setDefaultTimeout(0);
         
         await page.setViewport({ 
             width: 1600, 
@@ -94,14 +96,12 @@ describe('Taking Screenshots<3', () => {
         const numberTimes = Math.floor(height/viewportHeight);
         console.log(numberTimes);
 
-       
-
         //this code will scroll until the end of the page (full page)
         async function autoScroll(page){
             await page.evaluate(async () => {
                 await new Promise((resolve, reject) => {
                     var totalHeight = 0;
-                    var distance = 100;
+                    var distance = 500;
                     var timer = setInterval(() => {
                         var scrollHeight = document.body.scrollHeight;
                         window.scrollBy(0, distance);
@@ -116,7 +116,37 @@ describe('Taking Screenshots<3', () => {
             });
         }
 
+// scroll to end of page to load all sightloader slots
+        await autoScroll(page);
+        adSSetup = await page.evaluate("adSSetup");
+        adSlots = adSSetup["adPlacements"];
 
+        console.log("adSlots = ", adSlots)
+
+        for (var i = 0; i < adSlots.length; i++) {
+            console.log("current slot ", adSlots[i])
+            // check if slot exists
+            var selector = adSlots[i];
+            var slotFound = await page.evaluate( async (selector) => {
+                var slot = document.getElementById(selector);
+                return slot ? true : false
+            }, selector);
+
+            console.log("selector found ", slotFound)
+            // if slot exists - scroll into view & take screenshot
+            if (slotFound) {
+                await ScrollAdslotIntoView(page, adSlots[i]);
+                await page.waitForTimeout(1500);
+                console.log("a")
+                await page.screenshot({ path: "tests/screenshots/" + adSlots[i] + ".png" });
+                console.log("b")
+                await page.waitForTimeout(250);
+            } else {
+                console.log("no slot for " + adSlots[i] + " found");
+            }
+        }
+
+/* 
 //scrolling function (for by distance partial)
         async function autoScroll2(page, distance){
             await page.evaluate(async (distance) => {
@@ -138,6 +168,25 @@ describe('Taking Screenshots<3', () => {
                 })
 
             }
+        } */
+
+        async function ScrollAdslotIntoView(page, _adSlot) {
+            console.log("ScrollAdslotIntoView(" + _adSlot + ") was called");
+            await page.evaluate(async (_adSlot) => {
+                return await new Promise(resolve => {
+                    setTimeout(() => {
+                        console.log("looking for adslot " + _adSlot);
+                        var slot = document.getElementById(_adSlot);
+                        if (slot != null) {
+                            slot.scrollIntoView();
+                            return resolve({"found": true});
+                        } else {
+                            console.log(_adSlot + " not found");
+                            return resolve({"found": false});
+                        }
+                    }, 1000)
+                })
+            }, _adSlot);
         }
 
 
