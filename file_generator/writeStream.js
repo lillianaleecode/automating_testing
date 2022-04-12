1fd4a5dc9c395bebe78dad6f0c8cc570517772d1
lillianaleecode <lilliana.lee@code.berlin>
2022-04-12 15:51:53 +0200

    generate html file with json data

--- file_generator/writeStream.js
+++ file_generator/writeStream.js
@@ -1,18 +1,128 @@
 const fs = require('fs');
+// JSON data
+const data = require('./data.json');
+// Build paths
+const { buildPathHtml } = require('./buildPaths');
 
-/* fs.readFile('./file_generator/starter.txt', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-}) */
+/**
+ * Take an object which has the following model
+ * @param {Object} item 
+ * @model
+ * {
+ *   "invoiceId": `Number`,
+ *   "createdDate": `String`,
+ *   "dueDate": `String`,
+ *   "address": `String`,
+ *   "companyName": `String`,
+ *   "invoiceName": `String`,
+ *   "price": `Number`,
+ * }
+ * 
+ * @returns {String}
+ */
+const createRow = (item) => `
+  <tr>
+        <td>${item.testName}</td>
+        <td>${item.testId}</td>
+        <td>${item.testDate}</td>
+        <td>${item.testResult}</td>
+        <td>${item.notes}</td>
+        <td>${item.linkReference}</td>
+  </tr>
+`;
 
-const data = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+<tr>
 
-let wstream = fs.createWriteStream("write.html");
 
-wstream.write(data, (err) => {
-    if (err){
-        console.log(err.message);
-    }else{
-        console.log("data written");
-    }
-})
+</tr>
+
+/**
+ * @description Generates an `html` table with all the table rows
+ * @param {String} rows
+ * @returns {String}
+ */
+const createTable = (rows) => `
+  <table>
+    <tr>
+        <th>Test Name</td>
+        <th>Test Id</td>
+        <th>Test Date</td>
+        <th>Test Result</td>
+        <th>Notes</td>
+        <th>Link Reference</td>
+
+    </tr>
+    ${rows}
+  </table>
+`;
+
+/**
+ * @description Generate an `html` page with a populated table
+ * @param {String} table
+ * @returns {String}
+ */
+const createHtml = (table) => `
+  <html>
+    <head>
+      <style>
+        table {
+          width: 100%;
+        }
+        tr {
+          text-align: left;
+          border: 1px solid black;
+        }
+        th, td {
+          padding: 15px;
+        }
+        tr:nth-child(odd) {
+          background: #CCC
+        }
+        tr:nth-child(even) {
+          background: #FFF
+        }
+        .no-content {
+          background-color: red;
+        }
+      </style>
+    </head>
+    <body>
+      ${table}
+    </body>
+  </html>
+`;
+
+/**
+ * @description this method takes in a path as a string & returns true/false
+ * as to if the specified file path exists in the system or not.
+ * @param {String} filePath 
+ * @returns {Boolean}
+ */
+const doesFileExist = (filePath) => {
+	try {
+		fs.statSync(filePath); // get information of the specified file path.
+		return true;
+	} catch (error) {
+		return false;
+	}
+};
+
+try {
+	/* Check if the file for `html` build exists in system or not */
+	if (doesFileExist(buildPathHtml)) {
+		console.log('Deleting old build file');
+		/* If the file exists delete the file from system */
+		fs.unlinkSync(buildPathHtml);
+	}
+	/* generate rows */
+	const rows = data.map(createRow).join('');
+	/* generate table */
+	const table = createTable(rows);
+	/* generate html */
+	const html = createHtml(table);
+	/* write the generated html to file */
+	fs.writeFileSync(buildPathHtml, html);
+	console.log('Succesfully created an HTML table');
+} catch (error) {
+	console.log('Error generating table', error);
+}
