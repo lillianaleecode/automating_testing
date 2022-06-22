4a56e842155615215cbb7fd371ad9e00b9b58236
lillianaleecode <lilliana.lee@code.berlin>
2022-06-22 11:36:27 +0200

    refactor. separating specific spec/tests with it()

--- buildProtocol.js
+++ buildProtocol.js
@@ -236,66 +236,128 @@ describe('Create HTML Test Protocol ', () => {
 
    
 
-//3. CHECK FOR CONSOLE ERRORS
-//DESKTOP
-        await page.setViewport({ 
-            //Desktop size
-            width: 1920, 
-            height: 1800, 
-            });
-        await page.goto(url);
-        
-        removeCmpLayer(page);
-        
-        fs.appendFileSync('buildProtocol.html',"<h2> Check for console errors: </h2>  <h3> Errors from Adlib (1): <h/3> ", err => {if (err) {console.error(err)}}); 
 
-        const logs = [];
-        let text = page.on('console', async (msg) => {
-            var location = msg.location();
-            var source = "unknown";
-            if (typeof location == "object" && location.url != ""){
-                source = location.url;
-            }
-            console.log("console." + msg._type + "(" + msg._text + "(")
-            if (msg._type == "error"){
-                fs.appendFileSync('buildProtocol.html',  "<li>" + msg._type +" "+ msg._text +  "</li>" , err => {if (err) {console.error(err)}});;
-
-            }
-           
-        });
-        
-        for (let i = 0; i < logs.length; i++) {
-                text +=  logs[i]  ;
-            
-        }
-
-//3. MEDIATION CHECK
-
-        fs.appendFileSync('buildProtocol.html',"<h2> Mediation Check: </h2>", err => {if (err) {console.error(err)}});
-
-        await page.on("response", async (response) => {
-            
-            if (response._request._resourceType == "script" && response._url.includes("https://www.asadcdn.com/adlib/libmodules/extensions/mediation") ) {
-              console.log(" ✅  Source of our own Mediation Script:" + await response._url);
-
-              fs.appendFileSync('buildProtocol.html',  "<li>" + " ✅  Source of our own Mediation Script:" + await response._url +  "</li>" , err => {if (err) {console.error(err)}})
-              
-            } 
-            
-            if (response._request._resourceType == "script" && response._url.includes("https://acdn.adnxs-simple.com/ast/mediation/") ) {
-              console.log(" ❌ Default version source from Xandr Mediation Script:" + await response._url);
-
-              fs.appendFileSync('buildProtocol.html',  "<li>" + " ❌ Default version source from Xandr Mediation Script:" + await response._url +  "</li>" , err => {if (err) {console.error(err)}})
-              
-            } 
-          });
-
-
-          await page.goto(url)  
-
-        
 
     });
+    it('Mediation Script Check', async function() {
+        //Puppeteer basic config
+                const browser = await puppeteer.launch({
+                  headless: false, 
+                  devtools: true
+                })
+                const page = await browser.newPage() 
+        
+        // Configure the navigation timeout
+                this.timeout(0);
+        
+                await page.goto(url, {
+                     timeout: 0
+                  });
+        
+                
+        
+                await page.setDefaultTimeout(0);
+                
+                await page.setViewport({ 
+                    width: 1600, 
+                    height: 2000, 
+                   
+                });
+        
+        //remove the cmp layer so that mediation script will render.
+        //it is not working, as I need to remove the CMP manually :/ its like the page is rendering twice.
+        
+                async function removeCMP(){
+                    try {
+                        var frames = await page.frames();
+                        var cmpFrame = frames.find(
+                            f => f.url().indexOf("https://cmp2.bild.de/index.html") > -1); // return frame only if source matches
+                            if (cmpFrame == undefined) {
+                                console.log("can't find cmp frame");
+                            } else {
+                                const cmpButton = await cmpFrame.waitForSelector('button.message-component.message-button.no-children.focusable.sp_choice_type_11');
+                                await cmpButton.click();
+                            }
+                        
+                    } catch (err) {
+                         console.log("error getting the cmp button")
+                        // console.log(err);
+                        // process.exit();
+                    }
+        
+                }
+        
+                await removeCMP();
+                        
+        
+        //getting page response.
+        
+               
+        
+                await removeCMP();
+                page.on("response", async (response) => {
+                    
+                    if (response._request._resourceType == "script" && response._url.includes("https://www.asadcdn.com/adlib/libmodules/extensions/mediation") ) {
+                      console.log(" ✅  Source of our own Mediation Script:" + await response._url);
+        
+                      fs.appendFileSync('buildProtocol.html',  "<li>" + " ✅  Source of our own Mediation Script:" + await response._url +  "</li>" , err => {if (err) {console.error(err)}})
+                      
+                    } 
+                    
+                    if (response._request._resourceType == "script" && response._url.includes("https://acdn.adnxs-simple.com/ast/mediation/") ) {
+                      console.log(" ❌ Default version source from Xandr Mediation Script:" + await response._url);
+        
+                      fs.appendFileSync('buildProtocol.html',  "<li>" + " ❌ Default version source from Xandr Mediation Script:" + await response._url +  "</li>" , err => {if (err) {console.error(err)}})
+        
+                    }
+                  });
+        
+                   await page.goto(url)  
+                
+            });
+            
+    it('Check Console Errors', async function() {
+        //Puppeteer basic config
+        const browser = await puppeteer.launch({
+            headless: false, 
+            devtools: true
+          })
+          const page = await browser.newPage() 
+        //3. CHECK FOR CONSOLE ERRORS
+//DESKTOP
+await page.setViewport({ 
+    //Desktop size
+    width: 1920, 
+    height: 1800, 
+    });
+await page.goto(url);
+
+removeCmpLayer(page);
+
+fs.appendFileSync('buildProtocol.html',"<h2> Check for console errors: </h2>  <h3> Errors from Adlib (1): <h/3> ", err => {if (err) {console.error(err)}}); 
+
+const logs = [];
+let text = page.on('console', async (msg) => {
+    var location = msg.location();
+    var source = "unknown";
+    if (typeof location == "object" && location.url != ""){
+        source = location.url;
+    }
+    console.log("console." + msg._type + "(" + msg._text + "(")
+    if (msg._type == "error"){
+        fs.appendFileSync('buildProtocol.html',  "<li>" + msg._type +" "+ msg._text +  "</li>" , err => {if (err) {console.error(err)}});;
+
+    }
+   
+});
+
+for (let i = 0; i < logs.length; i++) {
+        text +=  logs[i]  ;
+    
+}
+
+    })
+
 
     
 })
