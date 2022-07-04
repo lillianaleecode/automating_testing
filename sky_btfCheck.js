bc2819169a10e5c7fbe0e56b6167163886f9485f
lillianaleecode <lilliana.lee@code.berlin>
2022-07-04 14:48:38 +0200

    progress on skybtf check and organizing code with better indentations

--- sky_btfCheck.js
+++ sky_btfCheck.js
@@ -1,3 +1,4 @@
+'use strict';
 const puppeteer = require("puppeteer");
 
 var url = 'https://www.bild.de/';
@@ -26,68 +27,115 @@ async function removeCmpLayer(page){
 
   describe('Puppeteer for AdTech', () => {
       
-    it('Check AdSlot sky_btf', async function() {
+        it('Check AdSlot sky_btf', async function() {
 
-    const browser = await puppeteer.launch({
-        headless: false,
-        devtools: true,
-    })
-    const page = await browser.newPage() 
+        const browser = await puppeteer.launch({
+            headless: false,
+            devtools: true,
+        })
+        const page = await browser.newPage() 
 
-    this.timeout(0);
+        this.timeout(0);
 
-    await page.setDefaultTimeout(0);
-
-
-    await page.setViewport({ 
-        //Desktop size
-        width: 1920, 
-        height: 1800, 
-        });
-    await page.goto(url);
-    removeCmpLayer(page);
+        await page.setDefaultTimeout(0);
 
 
-    // Check that the page min height has to be 5000px
-    const htmlHeight = await page.evaluate(() => {
-
-        return document.getElementById("__layout").offsetHeight;    
-    });
-    if (htmlHeight <= 5000){
-        console.log(" ❌ Page's height is: " + htmlHeight + " minimum height of 5000px is not fulfilled; therefore, no enough place for two Sky slots.")
-    }
-    console.log("1) Page's height is: " + htmlHeight);
-
-    if (htmlHeight > 5000){
-        console.log(" ✅  Page's height is: " + htmlHeight + ". minimum height of 5000px is fulfilled; therefore, there is enough place for two Sky slots.")
-    }
+        await page.setViewport({ 
+            //Desktop size
+            width: 1920, 
+            height: 1800, 
+            });
+        await page.goto(url);
+        removeCmpLayer(page);
 
 
-    //array to find: 
-    adSlots = ['sky',
-    'sky_btf']
-    console.log("adSlots array = ", adSlots)
-
-   //1a. check if Sky is found in adSSetup DESKTOP
-        
-   for (var i = 0; i < adSlots.length; i++) {
-       console.log("current slot ", adSlots[i])
-    
-       var selector = adSlots[i];
-       var slotFound = await page.evaluate( async (selector) => {
-           var slot = document.getElementById(selector);
-           return slot ? true : false
-       }, selector);
-
-       console.log("selector found ", slotFound)
-       
-       if (slotFound) {
-          console.log("slot found :)");
             
-       } 
-   }
-   
+            //array to find: 
+        const adSlots = ['sky',
+            'sky_btf']
+            console.log("adSlots array = ", adSlots)
 
-    
-});
+        //1a. check if Sky is found in adSSetup DESKTOP
+                
+        for (var i = 0; i < adSlots.length; i++) {
+            console.log("current slot ", adSlots[i])
+            
+            var selector = adSlots[i];
+            var slotFound = await page.evaluate( async (selector) => {
+                var slot = document.getElementById(selector);
+                return slot ? true : false
+            }, selector);
+
+            console.log("selector found ", slotFound)
+            
+            if (slotFound) {
+                console.log("slot found :)");
+                    
+            } 
+        }
+
+        // Check that the page min height has to be 5000px
+        const htmlHeight = await page.evaluate(() => {
+
+            return document.getElementById("__layout").offsetHeight;    
+            });
+
+            if (htmlHeight <= 5000){
+                console.log(" ❌ Page's height is: " + htmlHeight + " minimum height of 5000px is not fulfilled; therefore, no enough place for two Sky slots.")
+            }
+            console.log("1) Page's height is: " + htmlHeight);
+
+            if (htmlHeight > 5000){
+                console.log(" ✅  Page's height is: " + htmlHeight + ". minimum height of 5000px is fulfilled; therefore, there is enough place for two Sky slots.")
+            }
+
+            // Check Sky_btf placement (should not be rendered earlier than 2500px from the top of the page)
+
+        //   //code not working:
+        //     const skyPosition = await page.evaluate(() => {
+
+        //         return document.getElementById("sky");   
+        //     });
+
+        //    const SkyCheck = await page.evaluate(() => {
+        //       return  console.log(document.getElementById("sky_btf").getBoundingClientRect());
+
+        // });
+
+        // code works:)
+
+            async function positionDOMElement(selector, padding = 0) {
+                const rect = await page.evaluate(selector => {
+                const element = document.querySelector(selector);
+                const { x, y, width, height } = element.getBoundingClientRect();
+                return { left: x, top: y, width, height, id: element.id };
+                }, selector);
+                console.log('rect: ', rect);
+
+                
+
+                // return page.on({
+                //   clip: {
+                //     x: rect.left - padding,
+                //     y: rect.top - padding,
+                //     width: rect.width + padding * 2,
+                //     height: rect.height + padding * 2,
+                //   },
+                // });
+
+            }
+        
+        // call function (Rect position of DOM elements):
+        await positionDOMElement('#sky', 1);
+        await positionDOMElement('#sky_btf', 1);
+
+
+
+        
+
+
+        
+    });
+
+
 })
